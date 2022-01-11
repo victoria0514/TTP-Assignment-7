b405e51bb4d0a138cf55cb690dfabf3a4436d89b
Victoria Jaczynski <victoria.jacz.14@gmail.com>
2022-01-11 14:32:38 -0500

    init

--- src/App.js
+++ src/App.js
@@ -1,7 +1,33 @@
 import logo from './logo.svg';
+import React from "react";
 import './App.css';
 
 function App() {
+
+  const fetchData = () => {
+    return fetch(" http://api.giphy.com/v1/gifs/search?q=SEARCH+TERM+GOES+HERE&api_key=NktVZIpFmrTj0MkgLNgOpkSlNpqzNg7a")
+          .then((response) => response.json())
+          .then((data) => console.log(data));}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
   return (
     <div className="App">
       <header className="App-header">
