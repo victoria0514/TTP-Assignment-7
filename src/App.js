02c22eb00acfa1a0fa8c9e19ccca5fdd447f04fd
Victoria Jaczynski <victoria.jacz.14@gmail.com>
2022-01-11 15:55:59 -0500

    update, but debating on things in comments.

--- src/App.js
+++ src/App.js
@@ -1,32 +1,50 @@
 import logo from './logo.svg';
-import React from "react";
+import React, { useState, useEffect } from "react";
+// import React from "react";
+import axios from "axios"
 import './App.css';
 
-function App() {
+//function App() {
+export default function App() {
+     const [data, setData] = useState(null);
+     const [loading, setLoading] = useState(true);
+     const [error, setError] = useState(null);
+    
+    useEffect(() => {
+    axios("http://api.giphy.com/v1/gifs/search?q=SEARCH+TERM+GOES+HERE&api_key=NktVZIpFmrTj0MkgLNgOpkSlNpqzNg7a")
+    .then((response) => {
+    setData(response.data);
+    })
 
-  const fetchData = () => {
-    return fetch(" http://api.giphy.com/v1/gifs/search?q=SEARCH+TERM+GOES+HERE&api_key=NktVZIpFmrTj0MkgLNgOpkSlNpqzNg7a")
-          .then((response) => response.json())
-          .then((data) => console.log(data));}
+    .catch((error) => {
+    console.error("Error fetching data: ", error);
+    setError(error);
+    })
+ 
+    .finally(() => {
+    setLoading(false);
+    });
+    }, []);
+
+// Code Adapted From: https://dev.to/olenadrugalya/ways-of-getting-data-from-api-in-react-2kpf (above but not sure if I want to do by axios or fetch API)
+// NOTE: above is axios
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// going to use this as a hook. not done yet, needs more research
+// componentDidMount()
+// {
+//   fetch("http://api.giphy.com/v1/gifs/search?q=SEARCH+TERM+GOES+HERE&api_key=NktVZIpFmrTj0MkgLNgOpkSlNpqzNg7a")
+//   .then((response) => {
+//     setData(response.data);
+//     })
+//     .then((json) => {
+//       console.log(json);
+//       let x = json.data(() => {
+//         return data;
+//       });
+//       console.log(x);
+//     });
+// }
 
   return (
     <div className="App">
